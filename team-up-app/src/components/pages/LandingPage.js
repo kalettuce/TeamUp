f35790cee3931fbabaa4f29b572d93db59b536f6
Brandon Liu <brandonliu00@gmail.com>
2021-05-11 15:39:16 -0700

    Moved Navigation bar to container folder

--- team-up-app/src/components/pages/LandingPage.js
+++ team-up-app/src/components/pages/LandingPage.js
@@ -5,7 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
-import NavigationBar from '../NavigationBar';
+import NavigationBar from '../containers/NavigationBar';
 import { useRouteChanger } from '../../utils/RouteChanger';
 
 function LandingPage() {
