bb97ae6cc26ca0df45ad70860e820ca7ade4d90f
kalettuce <kjiangzk7@gmail.com>
2021-05-20 13:54:48 +0800

    Added actual project creation function to CreateAProject page

---
+++ team-up-app/src/utils/AddProjects.js
@@ -0,0 +1,24 @@
+import firebase from './Firebase.js';
+
+const database = firebase.database();
+
+export function addAProject(name, tagline, time_zone, description, application, tags) {
+  let tagList = tags.split(';').map((tag) => tag.trim());
+  let projData =  {
+    name: name,
+    tagline: tagline,
+    time_zone: time_zone,
+    description: description,
+    application: application,
+    tags: tagList,
+    members: [],
+    invitations_sent: []
+  };
+
+  let newPid = database.ref().child('projects').push().key;
+
+  let updates = {};
+  updates['/projects/' + newPid] = projData;
+
+  return database.ref().update(updates);
+}
