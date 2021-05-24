aa8291eadddf9f2a31af8e34736c0206fe2ceb1e
Chandra <burnhc@uw.edu>
2021-05-23 18:34:45 -0700

    Changed main material ui palette color to black

--- team-up-app/src/App.js
+++ team-up-app/src/App.js
@@ -2,12 +2,16 @@ import Router from "./Router";
 import './App.css';
 import { AuthProvider } from "./utils/AuthContext";
 import NavigationBar from "./components/containers/NavigationBar";
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from "./ThemePalette";
 
 function App(props) {
   return (
     <AuthProvider>
-      <NavigationBar />
-      <Router />
+      <ThemeProvider theme={theme}>
+        <NavigationBar />
+        <Router />
+      </ThemeProvider>
     </AuthProvider>
   );
 }
