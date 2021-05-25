748a9c2e30ee8ba1e3086cdaa08fc5f13b71737f
Brian Yao <byao9310@gmail.com>
2021-05-25 14:07:59 -0700

    fixed adding requests

--- team-up-app/src/utils/AddJoinRequests.js
+++ team-up-app/src/utils/AddJoinRequests.js
@@ -8,40 +8,6 @@ export function addRequest(uid, pid, message) {
     // adding the key itself to the fields
     database.ref('requests').child(rid).update({key: rid});
 
-    //database.ref("projects").child(pid).child("requests_received").push(rid);
-    //database.ref("users").child(uid).child("requests_sent").push(rid);
-
-    // update the field in projects
-    database.ref(`/projects/${pid}/requests_received`).once('value')
-        .then((snapshot) => {
-            var path;
-            var updates = {};
-            if (snapshot.val() !== null) { // create the object if it's null
-                console.log("exists in p");
-                path = `/projects/${pid}/requests_received/`;
-                updates[rid] = uid;
-            } else {
-                console.log("not exists in p");
-                path = `/projects/${pid}`;
-                updates['requests_received'] = {};
-                updates['requests_received'][rid] = uid;
-            }
-            database.ref(path).update(updates);
-        });
-
-    // update the field in users
-    database.ref(`/users/${uid}/requests_sent`).once('value')
-        .then((snapshot) => {
-            var path;
-            var updates = {};
-            if (snapshot.val() !== null) {
-                path = `/users/${uid}/requests_sent`;
-                updates[rid] = pid;
-            } else {
-                path = `/users/${uid}`;
-                updates['requests_sent'] = {};
-                updates['requests_sent'][rid] = pid;
-            }
-            database.ref(path).update(updates);
-        });
+    database.ref("projects").child(pid).child("requests_received").child(rid).set(uid);
+    database.ref("users").child(uid).child("requests_sent").child(rid).set(pid);
 }
