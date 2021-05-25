c23a9104c65452bf9efad063a3e2cd8f34ae0f38
Brian Yao <byao9310@gmail.com>
2021-05-25 14:01:15 -0700

    Function for adding user images

--- team-up-app/src/utils/FileStorage.js
+++ team-up-app/src/utils/FileStorage.js
@@ -62,3 +62,29 @@ export function removeProjectFolder(pid) {
     const path = `/projects/${pid}`;
     deleteFolderContents(path);
 }
+
+// Upload and set the image for a project
+// uid: the user id
+// file: the image to upload
+// callback: calls this when done, no arguments
+// Once set, the image can be accessed through the database under users/uid/image_url
+export function setUserImage(uid, file, callback) {
+    const path = "users/" + uid + "/";
+
+    if (file.length !== 0) {
+        // Get the old image url
+        console.log(path, file);
+        database.ref(path).child("image_url").once("value")
+                .then(snapshot => { return snapshot.val(); })
+                // Delete the old image
+                .then(url => storage.refFromURL(url).delete())
+                .catch(() => { /* No old image to delete, not an error */ })
+                // Upload the new image
+                .then(() => storage.ref(path).child(file.name).put(file))
+                // Update the image url in the database
+                .then(snapshot => snapshot.ref.getDownloadURL())
+                .then(url => database.ref(path).child("image_url").set(url))
+                .catch(console.error)
+                .then(() => callback());
+    }
+}
