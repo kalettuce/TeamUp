ecde41e8dac439aee0c0a60cb6e29efa59a6acaf
Brian Yao <byao9310@gmail.com>
2021-05-31 02:07:37 -0700

    Added functions to fetch projects that a user is involved with

--- team-up-app/src/utils/FindProjects.js
+++ team-up-app/src/utils/FindProjects.js
@@ -19,3 +19,53 @@ export function fetchProjectById(id, callback) {
         callback(snapshot.val() ? snapshot.val() : NOT_FOUND);
     });
 }
+
+// Fetch all projects owned by the user
+// Returns an array of project objects
+// If the user owns no projects, returns an empty array
+export function fetchProjectsByOwner(uid, callback) {
+    database.ref('/users/' + uid + '/owned_projects/')
+            .once('value')
+            .then((snapshot) => {
+                const pids = snapshot.val();
+
+                if (pids === null) {
+                    return [];
+                }
+
+                const promises = [];
+                for (let pid of pids) {
+                    const promise = database.ref('/projects/' + pid + '/')
+                                            .once('value')
+                                            .then(snapshot => snapshot.val());
+                    promises.push(promise);
+                }
+
+                Promise.all(promises).then(callback);
+            });
+}
+
+// Fetch all projects that a user is a member of
+// Returns an array of project objects
+// If the user is in no projects, returns an empty array
+export function fetchProjectsByMember(uid, callback) {
+    database.ref('/users/' + uid + '/joined_projects/')
+            .once('value')
+            .then((snapshot) => {
+                const pids = snapshot.val();
+
+                if (pids === null) {
+                    return [];
+                }
+
+                const promises = [];
+                for (let pid of pids) {
+                    const promise = database.ref('/projects/' + pid + '/')
+                                            .once('value')
+                                            .then(snapshot => snapshot.val());
+                    promises.push(promise);
+                }
+
+                Promise.all(promises).then(callback);
+            });
+}
